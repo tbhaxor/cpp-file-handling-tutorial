9d9fab8487d70566874efae6d6be4cfb222f8713
Gurkirat Singh <tbhaxor@gmail.com>
2022-04-02 16:35:51 +0530

    add WritingFile Project

---
+++ WritingFile/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    std::cerr << "usage: " << argv[0] << " <filename> <text1>\n";
+    return 0x1;
+  }
+
+  std::ofstream *file = new std::ofstream();
+
+  file->open(argv[1], std::ios::out); // open file with out mode
+
+  if (file->is_open()) {
+    (*file) << argv[2] << std::endl; // deref the pointer and write into file
+  } else {
+    std::cerr << "Failed to open file\n";
+  }
+
+  file->close(); // close the handle
+
+  delete file;
+  file = nullptr;
+  return 0x0;
+}
